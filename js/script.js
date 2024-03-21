e0152064a9ca04c30a0771446e254ed9013a6b3b
achmad rusdiana <achmad.rusdi123@gmail.com>
2024-03-21 22:33:54 +0700

    init commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+  
+  function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+  }
+  
+  function calculate() {
+    let expression = document.getElementById('display').value;
+    let result;
+    try {
+      result = eval(expression);
+    } catch (error) {
+      result = 'Error';
+    }
+    document.getElementById('display').value = result;
+  }
+  
